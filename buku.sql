c435d181cf281bbdab1bdd85b79a7b13440c0c46
IkkiRama <georgeikkirama@gmail.com>
2022-03-25 08:33:06 +0700

    ubah database

--- buku.sql
+++ buku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Mar 2022 pada 07.14
+-- Waktu pembuatan: 25 Mar 2022 pada 02.32
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -44,7 +44,28 @@ CREATE TABLE `buku` (
 INSERT INTO `buku` (`id_buku`, `judul`, `pengarang`, `penerbit`, `gambar`, `created_at`, `updated_at`) VALUES
 (3, 'kimak', 'kimak', 'kimak', '623421091cba9.jpg', '2022-03-18 06:13:55', '2022-03-18 06:04:57'),
 (4, 'anjay', 'anjay', 'anjay', '6234217ef28e8.jpg', '2022-03-18 06:14:11', '2022-03-18 06:06:54'),
-(5, 'lol', 'sdf', 'sdf', '623421b7734d8.jpg', '2022-03-18 06:12:56', '2022-03-18 06:07:51');
+(10, 'zxczxc', 'zxc', 'zxc', '623d1846d39a5.jpg', '2022-03-25 01:17:58', '2022-03-25 01:17:58');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `email`, `password`) VALUES
+(2, 'a', 'a@gmail.com', '$2y$10$ox2FIQGUKRxhXe9Wo7oOcuAz8QgzHl4lLKBnpOZIjii'),
+(3, 'anjay', 'anjay@gmail.com', '$2y$10$eInF4YAVXDA0ruMHG3U7VOVQIyBPL.Qmbu2yO5fMmRI');
 
 --
 -- Indexes for dumped tables
@@ -57,6 +78,12 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
 
 --
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -64,7 +91,13 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
